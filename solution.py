d1a6f6c2ce27f5c111bcc74a4146dc1e7499b54e
yaj <yajneshvar.arumugam@gmail.com>
2017-05-22 17:49:14 -0400

    Adding functions to encode grid

--- solution.py
+++ solution.py
@@ -1,5 +1,20 @@
 assignments = []
 
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [a+b for a in A for b in B]
+
+boxes = cross('ABCDEFGHI','123456789')
+row_units = [cross(alp,'123456789') for alp in 'ABCDEFGHI']
+column_units = [cross(col,'ABCDEFGHI') for col in '123456789']
+square_units = [cross(row,col) for row in ('ABC','DEF','GHI') for col in ('123','456','789')]
+diagonal_unit1 = list(map(lambda x:x[0]+x[1],zip('ABCDEFGHI','123456789')))
+diagonal_unit2 = list(map(lambda x:x[0]+x[1],zip('ABCDEFGHI','987654321')))
+diagonal_units = [diagonal_unit1,diagonal_unit2]
+unitlist = row_units + column_units + square_units + diagonal_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
@@ -27,10 +42,6 @@ def naked_twins(values):
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
-
 def grid_values(grid):
     """
     Convert grid into a dict of {square: char} with '123456789' for empties.
@@ -41,7 +52,14 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    def replaceEmpties(x):
+        if x == '.':
+            return '123456789'
+        else:
+            return x
+    list_iter = map(replaceEmpties,grid)
+    grid_iter = zip(boxes,list_iter)
+    return dict(grid_iter)
 
 def display(values):
     """
@@ -49,19 +67,38 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    def itemKey(item):
+        return item[1]
+    longest_box = max(map(lambda x: len(x[1]),values.items()))
+    max_width = 1 + longest_box
+    seperator = '-'*(max_width*3)
+    line = seperator + '+' + seperator + '+' + seperator
+    for row in 'ABCDEFGHI':
+        print(''.join(values[row+col].center(max_width)+('|' if col in '36' else '')for col in '123456789'))
+        if row in 'CF':
+            print(line)
 
 def eliminate(values):
-    pass
+    for (k,v) in values.items():
+        if len(v) == 1:
+            for p in peers[k]:
+                if len(values[p]) > 1:
+                    curr_set = set(v)
+                    new_set = set(values[p])
+                    diff_set = new_set.difference(curr_set)
+                    values[p] = reduce(lambda x,y:x+y,diff_set,'')
+        else:
+            continue
+    return values
 
 def only_choice(values):
-    pass
+    return values
 
 def reduce_puzzle(values):
-    pass
+    return values
 
 def search(values):
-    pass
+    return values
 
 def solve(grid):
     """
@@ -72,6 +109,7 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    return grid_values(grid)
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
